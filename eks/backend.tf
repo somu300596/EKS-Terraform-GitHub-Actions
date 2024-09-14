5f1430a2f18c951919b55fe6019479a8b3d1fddf
Somu <32243122+somu300596@users.noreply.github.com>
2024-09-14 17:47:08 +0200

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-bucket1-somu"
+    bucket         = "somu-ews-bucket1"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
